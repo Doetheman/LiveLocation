81b6426d548760ce9a77ee3b8ccb426189770cfe
Dorian Holmes <restlessd@fb.com>
2018-07-17 23:30:22 -0700

    Initial Commit

---
+++ LiveLocation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LiveLocation
+//
+//  Created by Dorian Holmes on 7/17/18.
+//  Copyright © 2018 Dorian Holmes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LiveLocation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LiveLocation
+//
+//  Created by Dorian Holmes on 7/17/18.
+//  Copyright © 2018 Dorian Holmes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
